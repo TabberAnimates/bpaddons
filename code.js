48ba6be92e2ecc9aaf72d041b53676222b054895
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-11-10 21:15:53 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,18 @@
+function add(id, type, str) {
+const para = document.createElement("div"); // Create element
+para.innerHTML = str;
+if (type < 1) {
+let e = document.id.appendChild(para);
+} else if (type === 1) {
+let e = document.getElementById(id).appendChild(para);
+} else if (type === 2) {
+document.getElementsByTagName(id).appendChild(para);
+} else if (type === 3) {
+document.getElementsByClassName(id).appendChild(para);
+}
+}
+
+add("sidebarButtons", 1, `<button style="" class="sidebarButton" id="bp-addons" onclick="openPage()"><div class="sidebarButtonImg"><svg viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M126.5 149C138.374 149 148 139.374 148 127.5C148 115.626 138.374 106 126.5 106C114.626 106 105 115.626 105 127.5C105 139.374 114.626 149 126.5 149Z" fill="var(--themeColor)"></path><path d="M101 64L108.972 12.846C109.048 12.3591 109.467 12 109.96 12H144.04C144.533 12 144.952 12.3591 145.028 12.846L153 64" stroke="var(--themeColor)" stroke-width="20"></path><path d="M153 192L145.028 243.154C144.952 243.641 144.533 244 144.04 244H109.96C109.467 244 109.048 243.641 108.972 243.154L101 192" stroke="var(--themeColor)" stroke-width="20"></path><path d="M84.5744 182.517L36.2877 201.19C35.8281 201.367 35.3074 201.184 35.061 200.757L18.0211 171.243C17.7747 170.816 17.876 170.274 18.2598 169.964L58.5744 137.483" stroke="var(--themeColor)" stroke-width="20"></path><path d="M169.426 73.4833L217.712 54.8103C218.172 54.6326 218.693 54.8162 218.939 55.243L235.979 84.757C236.225 85.1838 236.124 85.7265 235.74 86.0357L195.426 118.517" stroke="var(--themeColor)" stroke-width="20"></path><path d="M195.426 137.483L235.74 169.964C236.124 170.274 236.225 170.816 235.979 171.243L218.939 200.757C218.693 201.184 218.172 201.367 217.712 201.19L169.426 182.517" stroke="var(--themeColor)" stroke-width="20"></path><path d="M58.5744 118.517L18.2598 86.0357C17.876 85.7265 17.7747 85.1838 18.0211 84.757L35.061 55.243C35.3074 54.8162 35.8281 54.6326 36.2877 54.8103L84.5744 73.4833" stroke="var(--themeColor)" stroke-width="20"></path><path d="M194 128C194 165.003 164.003 195 127 195C89.9969 195 60 165.003 60 128C60 90.9969 89.9969 61 127 61C164.003 61 194 90.9969 194 128Z" stroke="var(--themeColor)" stroke-width="20"></path></svg></div> BP Addons </button>`);
+function openPage() {
+add("pageHolder", 3, `<div class="settingsSection" id="themeCodeMaker"> <div class="settingsTitle"> Scrollbar Color </div> <input class="settingsInput" id="scrollbarColor" style="margin-bottom:8px;" placeholder="#300d20"> <div class="settingsTitle"> Background Color </div> <input class="settingsInput" id="bgColor" style="margin-bottom:8px;" placeholder="#17060f80"> <div class="settingsTitle"> Miscellaneous Color #1 </div> <input class="settingsInput" id="misc1" style="margin-bottom:8px;" placeholder="#290b1b"> <div class="settingsTitle"> Miscellaneous Color #2 </div> <input class="settingsInput" id="misc2" style="margin-bottom:8px;" placeholder="#2e0c1e"> <div class="settingsTitle"> Miscellaneous Color #3 </div> <input class="settingsInput" id="misc3" style="margin-bottom:8px;" placeholder="#380f25"> <div class="settingsTitle"> Border Color </div> <input class="settingsInput" id="borderColor" style="margin-bottom:8px;" placeholder="#42122c"> <div class="settingsTitle"> Font Color </div> <input class="settingsInput" id="fontColor" style="margin-bottom:8px;" placeholder="#ffffff"> <div class="settingsTitle"> Theme Color </div> <input class="settingsInput" id="themeColor" style="margin-bottom:8px;" placeholder="#FF42A7"> <div class="settingsSaveHolder"><button class="settingsSave" id="generateCode" onclick="codeGen()">Generate</button></div> </div>`)
+}
